2a9d7454c9200a4d52916406c3d37ffef510edb2
Koramit Pichanaharee <koramit.p@gmail.com>
2018-07-30 10:33:54 +0700

    refactor InputState component

--- resources/assets/js/components/inputs/InputState.vue
+++ resources/assets/js/components/inputs/InputState.vue
@@ -1,147 +1,132 @@
 <template>
-    <div :class="divState">
-        <label for="" class="control-label">{{ label }}</label>
-        <input type="text"
-               class="form-control"
-               @input="oninput()"
-               @blur="checkState()"
-               @focus="focus()"
-               v-model="userInput"
-               :disabled="inputDisable">
-        <span :class="iconStateClass"></span>
-        <span class="help-block"><i>{{ helpText }}</i></span>
+    <div :class="state.themeClass"><!-- class provide state border color -->
+        <label class="control-label">{{ label }}</label>
+        <input 
+            type="text"
+            class="form-control"
+            :value="value"
+            :disabled="disabled"
+            @input="oninput"
+            @keydown="onkeydown"
+            ref="input" />
+        <span :class="state.iconClass"></span><!-- class provide state icon -->
+        <span class="help-block" v-if="state.text != ''"><!-- show help text if necessary -->
+            <i>{{ state.text }}</i>
+        </span>
     </div>
 </template>
 
 <script>
     export default {
         props: {
-            label: { default: 'label'},
-            field: { default: 'data' },
-            serviceUrl: { default: null },
-            initHelpText: { default: null },
-            pattern: { default: '.' },
-            inputValue: {
-                type: String,
-                required: true
-            },
-            isValid: {
-                type: Boolean,
-                required: true
-            },
-            inputDisable: {
-                type: String,
-                required: false
-            }
+            value: { required: true },              // input's value
+            label: { default: 'label'},             // input's label
+            name: { default: 'data' },              // input's name 
+            serviceUrl: { default: null },          // endpoint for database validation if necessary
+            placeholderStateText: { default: '' },  // placholer as state text
+            pattern: { default: '.' },              // pattern for format validation
+            disabled: { default: false }             // disable input or not
         },
         data() {
             return {
-                divState: 'form-group-sm has-feedback',
-                helpText: '',
-                iconStateClass: 'form-control-feedback',
-                userInput: ''
+                state: {
+                    themeClass: 'form-group-sm has-feedback',
+                    iconClass: 'form-control-feedback',
+                    text: ''
+                }
             }
         },
         computed: {
             regex() {
-                if ( this.pattern !== undefined ) {
-                    if ( this.pattern == 'email' ) {
-                        return new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
-                    }
-                    return new RegExp(this.pattern)
+                if ( this.pattern == 'email' ) {
+                    return new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
                 }
-                return new RegExp(/./)
+                return new RegExp(this.pattern)
             }
         },
-        mounted() {
-            if ( this.initHelpText !== undefined ) {
-                this.helpText = this.initHelpText
-            }
-
-            this.onkeypress = _.debounce(() => {
-                this.checkState()
-            }, 800)
-
-            if ( this.inputDisable === undefined ) {
-                this.inputDisable = null
-            }
-
+        created () {
+            this.onkeydown = _.debounce(this.validate, 800)
+            this.state.text = this.placeholderStateText
         },
-            $infomed_host = config('app.infomed_host');
-            $infomed_username = config('app.infomed_username');
-            $infomed_password = config('app.infomed_password');
         methods: {
-            checkStateWithApi() {
-                axios.post(this.serviceUrl, {
-                        field: this.field,
-                        value: this.userInput
-                     })
-                     .then((response) => {
-                        let valid = false;
-                        this.divState = 'form-group-sm has-feedback has-' + response.data.state
-                        this.iconStateClass = 'glyphicon form-control-feedback '
-                        switch (response.data.state) {
-                            case 'success':
-                                this.setOKState()
-                                valid = true
-                                break
-                            case 'warning':
-                                this.iconStateClass += 'glyphicon-warning-sign'
-                                this.helpText = response.data.reply_text
-                                break
-                            case 'error':
-                                this.iconStateClass += 'glyphicon-remove'
-                                this.helpText = response.data.reply_text
-                                break
-                            default:
-                                this.iconStateClass += 'glyphicon-remove'
-                                this.helpText = 'Whoops, someting went wrong. Plase try again.'
-                                break
-                        }
-                        this.$emit('update:isValid', valid)
-                     })
-                     .catch((error) => {
-                        console.log(error)
-                     })
-            },
-            checkState() {
-                if ( this.userInput !== '' ) {
-                    this.$emit('update:inputValue', this.userInput)
-                    if ( this.userInput.match(this.regex) !== null ) {
-                        if ( this.serviceUrl !== undefined ) {
-                            this.checkStateWithApi()
+            validate() {
+                if ( this.$refs.input.value != '' ) {
+                    if ( this.$refs.input.value.match(this.regex) !== null ) { // valid pattern
+                        if ( this.serviceUrl ) { // perform backend validation if needed
+                            this.state.iconClass = 'fa fa-circle-o-notch fa-spin form-control-feedback'
+                            this.$emit('disabled', true)
+                            axios.post(this.serviceUrl, {
+                                    field: this.name,
+                                    value: this.$refs.input.value
+                                 })
+                                 .then((response) => {
+                                    this.$emit('disabled', false)
+                                    switch (response.data.state) {
+                                        case 'success':
+                                            this.setState({
+                                                theme: 'has-success',
+                                                icon: 'glyphicon glyphicon-ok',
+                                                text: '',
+                                                payload: response.data.payload
+                                            })
+                                            break
+                                        case 'warning':
+                                            this.setState({
+                                                    theme: 'has-warning',
+                                                    icon: 'glyphicon glyphicon-warning-sign',
+                                                    text: response.data.reply_text,
+                                                    payload: response.data.payload
+                                                })
+                                            break
+                                        case 'error':
+                                            this.setState({
+                                                    theme: 'has-error',
+                                                    icon: 'glyphicon glyphicon-remove',
+                                                    text: response.data.reply_text,
+                                                    payload: response.data.payload
+                                                })
+                                            break
+                                        default:
+                                            this.setState({ 
+                                                    theme: 'has-error',
+                                                    icon: 'glyphicon glyphicon-remove',
+                                                    text: 'Whoops, someting went wrong. Plase try again.'
+                                                })
+                                            break
+                                    }
+                                 })
+                                 .catch((error) => {
+                                    this.$emit('error', error)
+                                    this.$emit('disabled', false)
+                                    this.state.iconClass = 'form-control-feedback'
+                                 })
                         } else {
-                            this.setOKState()
-                            this.$emit('update:isValid', true)
+                            this.setState({ theme: 'has-success', icon: 'glyphicon glyphicon-ok', text: '' })
                         }
                     } else {
-                        this.divState = 'form-group-sm has-feedback has-error'
-                        this.iconStateClass = 'glyphicon form-control-feedback glyphicon-remove'
-                        this.helpText = 'invalid input format'
-                        this.$emit('update:isValid', false)
+                        this.setState({
+                                theme: 'has-error',
+                                icon: 'glyphicon glyphicon-remove',
+                                text: 'invalid input format'
+                            })
                     }
                 } else {
-                    this.focus()
-                    this.$emit('update:isValid', false)
+                    this.setState({ theme: '', icon: '', text: this.placeholderStateText })
                 }
             },
-            focus() {
-                this.divState = 'form-group-sm has-feedback'
-                this.iconStateClass = 'form-control-feedback'
-                if ( this.initHelpText !== null ) {
-                    this.helpText = this.initHelpText
-                }
+            oninput () {
+                this.setState({ theme: '', icon: '', text: this.placeholderStateText })
+                this.$emit('input', this.$refs.input.value)
             },
-            oninput() {
-                this.onkeypress()
-            },
-             onkeypress() {
-                // define on mounted
-            },
-            setOKState() {
-                this.divState = 'form-group-sm has-feedback has-success'
-                this.iconStateClass = 'glyphicon form-control-feedback glyphicon-ok'
-                this.helpText = ''
+            setState(state) {
+                this.state.themeClass = 'form-group-sm has-feedback ' + state.theme
+                this.state.iconClass = 'form-control-feedback ' + state.icon
+                this.state.text = state.text
+                if ( state.payload !== undefined ) {
+                    this.$emit('validated', (state.theme == 'has-success'), state.payload)
+                } else {
+                    this.$emit('validated', (state.theme == 'has-success'))
+                }                
             }
         }
     }

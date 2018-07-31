fd9022a800c0a3530698703cdbaf50bd84ad9a7e
Koramit Pichanaharee <koramit.p@gmail.com>
2018-07-31 21:36:38 +0700

    create csrf-token component

---
+++ resources/assets/js/components/forms/CsrfToken.vue
@@ -0,0 +1,14 @@
+<template>
+    <input 
+        type="hidden"
+        name="_token"
+        :value="token" />
+</template>
+
+<script>
+    export default {
+        created () {
+            this.token = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        }
+    }
+</script>

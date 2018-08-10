6c0255aa9f8c6acfb9580f86b768d293d8016989
koramit <koramit.p@gmail.com>
2018-08-10 11:11:08 +0700

    routine update

--- resources/views/draft/demo/page-text-watermark.blade.php
+++ resources/views/draft/demo/page-text-watermark.blade.php
@@ -1,5 +1,13 @@
 @extends('layout.vue-draft')
 
 @section('content')
-<input-rich-text></input-rich-text>
+<!-- <input-rich-text></input-rich-text> -->
+<toggle
+    id='toggle-gender'
+    true-label='ชาย'
+    false-label='หญิง'></toggle>
+<toggle
+    id='employee-type'
+    true-label='เผลอ'
+    false-label='ไม่เผลอ'></toggle>
 @endsection
